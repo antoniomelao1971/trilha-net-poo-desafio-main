db4d45fe3a89dee3a577477c76189d4371cc5737
Antonio Melao <80098757+antoniomelao1971@users.noreply.github.com>
2023-01-04 19:45:12 -0300

    Final Version
    Criando um Sistema e Abstraindo um Celular com POO em C#

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using DesafioPOO.Models;
+class Desafio_POO
+{
+    public static void Main()
+    {
+
+        // TODO: Realizar os testes com as classes Nokia e Iphone
+        Console.WriteLine("Smartphone Nokia");
+        Smartphone nokia = new Nokia("21999183033", "N32", "147258369", 2);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Nibbles");
+
+        Console.WriteLine("/n");
+
+        Console.WriteLine("Smartphone Iphone");
+        Smartphone iphone = new Iphone("21972873597", "iPhone 12", "357951456", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Telegram");
+    }
+}
